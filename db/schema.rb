65584f78a0f80e308375fcc7397d149e39843096
yokoe9073 <yokoe9073@gmail.com>
2020-10-24 13:15:43 +0900

    rails g migration AddUserIdToTweets user_id:integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_24_031827) do
+ActiveRecord::Schema.define(version: 2020_10_24_041504) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "thought"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
