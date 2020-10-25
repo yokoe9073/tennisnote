e2318587039f2df01e776c27df48f5a12af81c78
yokoe9073 <yokoe9073@gmail.com>
2020-10-25 10:29:03 +0900

    新規投稿が上に来るように

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,7 @@
 class TweetsController < ApplicationController
   before_action :move_to_index, except: [:index]
   def index
-    @tweets = Tweet.includes(:user)
+    @tweets = Tweet.includes(:user).order("created_at DESC")
   end
 
   def new
