6ebe21b4f9a98b844c4d344d39adccef4a635739
yokoe9073 <yokoe9073@gmail.com>
2020-10-27 07:49:45 +0900

    showアクション追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,7 @@
 class TweetsController < ApplicationController
   before_action :move_to_index, except: [:index]
+  before_action :set_tweet, only: [:edit, :show]
+
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC")
   end
@@ -26,10 +28,19 @@ class TweetsController < ApplicationController
     tweet.update(tweet_params)
   end
 
+  def show
+    @comment = Comment.new
+    @comments = @tweet.comments.includes(:user)
+  end
+
   private
   
   def tweet_params
     params.require(:tweet).permit(:thought).merge(user_id: current_user.id)
+  end
+  
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 
   def move_to_index
