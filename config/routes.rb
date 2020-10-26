9bd3bd05368458c25bec8c5b3b08056255575c87
yokoe9073 <yokoe9073@gmail.com>
2020-10-27 07:59:24 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   root to: 'tweets#index'
   resources :tweets do
     resources :comments
+    collection do
+      get 'search'
+    end
   end
   resources :users
 end
